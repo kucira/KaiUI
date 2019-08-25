2c290430d14c6042b00b3e30d1fd21bc121602e8
Ikhlas Risandy <ikhlas.risandy@bizzy.co.id>
2019-08-26 00:53:41 +0700

    prettify

--- src/views/GridView/GridView.jsx
+++ src/views/GridView/GridView.jsx
@@ -6,111 +6,110 @@ import './GridView.scss';
 const prefixCls = 'kai-grid-view';
 
 const GridView = React.memo(
-  props => {
-    const itemRefs = [];
+    props => {
+        const itemRefs = [];
 
-    const [activeItem, setActiveItem] = useState(0);
+        const [activeItem, setActiveItem] = useState(0);
 
-    const {
-      children,
-      onChangeIndex,
-      isActive,
-    } = props;
+        const {
+            children,
+            onChangeIndex,
+            isActive,
+        } = props;
 
-    const handleChangeIndex = itemIndex => {
-      setActiveItem(itemIndex);
-      onChangeIndex(itemIndex);
-    };
+        const handleChangeIndex = itemIndex => {
+            setActiveItem(itemIndex);
+            onChangeIndex(itemIndex);
+        };
 
-    const setFocusToIndex = useCallback(
-      index => 
-      {
-        
-        const node = itemRefs[index];
-        if(node !== null){
-            if(node.current !== null || !node.current){
-                node.current.focus();
-            }
-            
-        }
-        if(node !== null && node.current !== null){
-          node.current.scrollIntoView({
-            behavior: "smooth", 
-            block: "end", 
-            inline: "nearest"
-          });
-        }
-      },
-      [itemRefs]
-    );
+        const setFocusToIndex = useCallback(
+            index => {
 
-    const handleKeyDown = useCallback(
-      e => {
-        let index = activeItem;
-        if (!isActive) {
-          return;
-        }
+                const node = itemRefs[index];
+                if (node !== null) {
+                    if (node.current !== null || !node.current) {
+                        node.current.focus();
+                    }
 
-        switch (e.key) {
-          case 'ArrowUp':
-            // looping to bottom
+                }
+                if (node !== null && node.current !== null) {
+                    node.current.scrollIntoView({
+                        behavior: "smooth",
+                        block: "end",
+                        inline: "nearest"
+                    });
+                }
+            },
+            [itemRefs]
+        );
 
-            index = (index < 3) ? (itemRefs.length - 1) - index : index - 3;
-            setFocusToIndex(index);
-            break;
-          case 'ArrowDown':
-            // looping to top
-            index = (index > itemRefs.length - 3 && index < itemRefs.length - 1) ? 0 : index + 3;
-            setFocusToIndex(index);
-            break;
-            case 'ArrowLeft':
-                // looping to left
-                index = index - 1 >= 0 ? index - 1 : itemRefs.length - 1;
-                setFocusToIndex(index);
-            break;
-            case 'ArrowRight':
-                // looping to right
-                index = index + 1 < itemRefs.length ? index + 1 : 0;
-                setFocusToIndex(index);
-            break;
-          default:
-            break;
-        }
-      },
-      [isActive, activeItem, setFocusToIndex, itemRefs]
-    );
+        const handleKeyDown = useCallback(
+            e => {
+                let index = activeItem;
+                if (!isActive) {
+                    return;
+                }
 
-    useEffect(
-      () => {
-        document.addEventListener('keydown', handleKeyDown);
-        return () => document.removeEventListener('keydown', handleKeyDown);
-      },
-      [handleKeyDown]
-    );
+                switch (e.key) {
+                    case 'ArrowUp':
+                        // looping to bottom
 
-    useEffect(
-      () => {
-        if(isActive) {
-          setFocusToIndex(activeItem)
-        }
-      },
-      [isActive, setFocusToIndex, activeItem]
-    );
+                        index = (index < 3) ? (itemRefs.length - 1) - index : index - 3;
+                        setFocusToIndex(index);
+                        break;
+                    case 'ArrowDown':
+                        // looping to top
+                        index = (index > itemRefs.length - 3 && index < itemRefs.length - 1) ? 0 : index + 3;
+                        setFocusToIndex(index);
+                        break;
+                    case 'ArrowLeft':
+                        // looping to left
+                        index = index - 1 >= 0 ? index - 1 : itemRefs.length - 1;
+                        setFocusToIndex(index);
+                        break;
+                    case 'ArrowRight':
+                        // looping to right
+                        index = index + 1 < itemRefs.length ? index + 1 : 0;
+                        setFocusToIndex(index);
+                        break;
+                    default:
+                        break;
+                }
+            },
+            [isActive, activeItem, setFocusToIndex, itemRefs]
+        );
 
-    const gridItemCls = `${prefixCls}-item`
+        useEffect(
+            () => {
+                document.addEventListener('keydown', handleKeyDown);
+                return () => document.removeEventListener('keydown', handleKeyDown);
+            },
+            [handleKeyDown]
+        );
 
-    const renderChildren = () => {
-      let index = -1;
-      return React.Children.map(children, child => {
-        // Don't focus on separators
-        if (child.props.separatorText != null) {
-          return child;
-        }
-        index++;
-        const newRef = React.createRef();
-        itemRefs[index] = newRef;
-        return (
-            <div className={gridItemCls}>
+        useEffect(
+            () => {
+                if (isActive) {
+                    setFocusToIndex(activeItem)
+                }
+            },
+            [isActive, setFocusToIndex, activeItem]
+        );
+
+        const gridItemCls = `${prefixCls}-item`
+
+        const renderChildren = () => {
+            let index = -1;
+            return React.Children.map(children, child => {
+                // Don't focus on separators
+                if (child.props.separatorText != null) {
+                    return child;
+                }
+                index++;
+                const newRef = React.createRef();
+                itemRefs[index] = newRef;
+                return (
+                    <div className={gridItemCls}>
                 {
                     React.cloneElement(child, {
                         index,
@@ -119,28 +118,28 @@ const GridView = React.memo(
                         })
                 }       
             </div>
-        )
-      });
-    };
+                )
+            });
+        };
 
-    return (
-        <div className={prefixCls}>
+        return (
+            <div className={prefixCls}>
             {renderChildren()}
         </div>
-    );
-  }
+        );
+    }
 );
 
 GridView.propTypes = {
-  children: PropTypes.array.isRequired,
-  onChangeIndex: PropTypes.func,
-  // Refocus on tab change
-  isActive: PropTypes.bool,
+    children: PropTypes.array.isRequired,
+    onChangeIndex: PropTypes.func,
+    // Refocus on tab change
+    isActive: PropTypes.bool,
 };
 
 GridView.defaultProps = {
-  onChangeIndex: () => {},
-  isActive: true,
+    onChangeIndex: () => {},
+    isActive: true,
 };
 
 export default GridView;
