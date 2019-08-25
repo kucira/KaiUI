fe37658f141dfee04862fe86440adda93ba86c3e
Ikhlas Risandy <ikhlas.risandy@bizzy.co.id>
2019-08-26 00:55:36 +0700

    add softkey and fix styling

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -8,52 +8,66 @@ import './Input.scss';
 const prefixCls = 'kai-inp';
 
 const Input = React.memo(
-  props => {
-    const {
-      label,
-      focusColor,
-      forwardedRef,
-      index,
-      onFocusChange,
-      placeholder,
-      onInputChange,
-      value,
-      centerText,
-      softKeyManager,
-      centerCallback,
-    } = props;
+    props => {
+        const {
+            id,
+            label,
+            focusColor,
+            forwardedRef,
+            index,
+            onFocusChange,
+            placeholder,
+            onInputChange,
+            value,
+            centerText,
+            leftText,
+            rightText,
+            softKeyManager,
+            centerCallback,
+            leftCallback,
+            rightCallback,
+        } = props;
 
-    const [isFocused, setFocused] = useState(false);
-    const inputRef = useRef(null);
+        const [isFocused, setFocused] = useState(false);
+        const inputRef = useRef(null);
 
-    const itemCls = `${prefixCls}-container`;
-    const secondaryCls = `${prefixCls}-container-secondary`;
+        const itemCls = `${prefixCls}-container`;
+        const secondaryCls = `${prefixCls}-container-secondary`;
 
-    const handleFocusChange = useCallback(
-      isNowFocused => {
-        setFocused(isNowFocused);
-       if (isNowFocused) {
-        onFocusChange(index);
-        inputRef.current.focus();
-        softKeyManager.setSoftKeyTexts({
-          centerText: centerText,
-        });
-        softKeyManager.setSoftKeyCallbacks({
-            centerCallback:()=>{
-              centerCallback();
-            }
-        });
-      }
-      else{
-        inputRef.current.blur();
-      }
-      },
-      [index, onFocusChange, centerCallback, centerText, softKeyManager]
-    );
+        const handleFocusChange = useCallback(
+            isNowFocused => {
+                setFocused(isNowFocused);
+                if (isNowFocused) {
+                    onFocusChange(index);
+                    inputRef.current.focus();
+                    softKeyManager.setSoftKeyTexts({
+                        centerText: centerText,
+                        leftText: leftText,
+                        rightText: rightText,
+                    });
+                    softKeyManager.setSoftKeyCallbacks({
+                        centerCallback: () => {
+                            centerCallback();
+                        },
+                        leftCallback: () => {
+                            leftCallback();
+                        },
+                        rightCallback: () => {
+                            rightCallback();
+                        }
+                    });
+                } else {
+                    inputRef.current.blur();
+                }
+            },
+            [index, onFocusChange, centerCallback, rightCallback, 
+            leftCallback, centerText, rightText, 
+            leftText, softKeyManager]
+        );
 
 
-    return (
-      <div
+        return (
+            <div
         tabIndex="0"
         className={itemCls}
         style={{ backgroundColor: isFocused ? focusColor : colors.white }}
@@ -63,42 +77,51 @@ const Input = React.memo(
       >
         {label && (<span className={secondaryCls} 
                           style={{ color: isFocused ? colors.white : colors.black }}>{label}</span>)}
-        <input ref={inputRef}
+        <input  id={id}
+                ref={inputRef}
                 placeholder={placeholder}
                 value={value}
                 onChange={(evt) =>{
                   onInputChange(evt.target.value);
                 }} />
       </div>
-    );
-  }
+        );
+    }
 );
 
 Input.propTypes = {
-  label: PropTypes.string,
-  focusColor: PropTypes.string,
-  forwardedRef: PropTypes.oneOfType([
-    PropTypes.func,
-    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
-  ]),
-  index: PropTypes.number,
-  onFocusChange: PropTypes.func,
-  onInputChange: PropTypes.func,
-  placeholder: PropTypes.string,
-  value: PropTypes.string,
-  centerText: PropTypes.string,
-  centerCallback: PropTypes.func,
+    id: PropTypes.string,
+    label: PropTypes.string,
+    focusColor: PropTypes.string,
+    forwardedRef: PropTypes.oneOfType([
+        PropTypes.func,
+        PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+    ]),
+    index: PropTypes.number,
+    onFocusChange: PropTypes.func,
+    onInputChange: PropTypes.func,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+    centerText: PropTypes.string,
+    leftText: PropTypes.string,
+    rightText: PropTypes.string,
+    centerCallback: PropTypes.func,
+    leftCallback: PropTypes.func,
+    rightCallback: PropTypes.func,
 };
 
 Input.defaultProps = {
-  focusColor: colors.defaultFocusColor,
+    focusColor: colors.defaultFocusColor,
+    centerCallback: ()=>{},
+    leftCallback: ()=>{},
+    rightCallback: ()=>{},
 };
 
 export default React.forwardRef((props, ref) => (
-  <SoftKeyConsumer>
+    <SoftKeyConsumer>
   {context => (
     <Input softKeyManager={context} forwardedRef={ref} {...props} />
   )}
 </SoftKeyConsumer>
-  
+
 ));
