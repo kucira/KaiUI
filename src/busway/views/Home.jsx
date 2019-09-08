025ebdcfa735bcc62c2c8c885c257d9bd934899a
Ikhlas Risandy <ikhlas.risandy@bizzy.co.id>
2019-09-08 17:27:16 +0700

    save data to indexdb
    load data from indexdb & update data on background & add backspace key

--- src/busway/views/Home.jsx
+++ src/busway/views/Home.jsx
@@ -5,6 +5,7 @@ import { SoftKeyProvider } from '../../components/SoftKey/SoftKeyProvider';
 import { SoftKeyConsumer } from '../../components/SoftKey/withSoftKeyManager';
 import { withRouter } from 'react-router-dom';
 import axios from 'axios';
+import localforage from 'localforage';
 import Spinner from 'react-spinner-material';
 import ListView from '../../views/ListView/ListView';
 import Input from '../../components/Input/Input';
@@ -17,7 +18,7 @@ function Home(props) {
   const { history } = props;
   const [buswayList, setBuswayList] = useState([]);
   const [numberBusway, setNumberBusway] = useState([]);
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(false);
   let countDown = 15;
   let id = 0;
 
@@ -32,29 +33,56 @@ function Home(props) {
           clearInterval(id);
           id = null;
           const input = document.getElementById('SearchNumber');
-          const buswayFilter = numberBusway.filter(c => c.longName.toLowerCase().includes(input.value.toLowerCase()));
+          const buswayFilter = numberBusway.filter(c => 
+            c.longName.toLowerCase().includes(input.value.toLowerCase()) ||
+            c.name.toLowerCase().includes(input.value.toLowerCase())
+          );
           setBuswayList(buswayFilter);
         }
       }, 100);
     }
   }, [buswayList]);
 
-  const fetchData = async () => {
+  const getCacheData = async () => {
     try{
-      const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/get-data`);
-      const busway = data.schedulesByTransportId.find(d => d.transportName === 'Transjakarta');
-      console.log(busway);
-      setNumberBusway(busway.schedules);
-      setBuswayList(busway.schedules);
-      setLoading(false);
+      const buswayNumber = await localforage.getItem('numbers');
+      return buswayNumber;
     }
     catch(error){
+      window.location.reload();
+    }
+  }
+
+  const mappingData = (data) => {
+    const busway = data.schedulesByTransportId.find(d => d.transportName === 'Transjakarta');
+    setNumberBusway(busway.schedules)
+    setBuswayList(busway.schedules);
+    setLoading(false);
+  }
+
+  const fetchData = async () => {
+    try{
+      const dataCache = await getCacheData();
+      if(dataCache){
+        mappingData(dataCache);
+      }
+      else{
+        setLoading(true);
+      }
+      const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/get-data`);
+      mappingData(data);
+      localforage.setItem('numbers', data);
+ 
+    }
+    catch(error){
+      setLoading(false)
       alert(error);
     }
   }
   
   useEffect(() => {
     fetchData();
+    return () => { setBuswayList([]); setNumberBusway([])}
   }, [])
 
   const renderList = () => {
@@ -106,9 +134,9 @@ function Home(props) {
                   display:'flex',
                   justifyContent:'center',
                   alignItems:'center',
-                  height:'100vh'
+                  height:'80vh'
                 }}>
-                  <Spinner size={120} spinnerColor={"#333"} spinnerWidth={2} visible={true} />
+                  <Spinner size={50} spinnerColor={"#333"} spinnerWidth={2} visible={true} />
                 </div>
             )
           }
